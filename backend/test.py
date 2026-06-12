a27d7203dd4da4edad360d860fa427ded1579b44
Swetha Varma <swethakv05@gmail.com>
2026-06-12 15:00:47 +0530

    Improved beginner issue recommendations

--- backend/test.py
+++ backend/test.py
@@ -10,23 +10,46 @@ issues_url = f"https://api.github.com/repos/{repo}/issues"
 issues_response = requests.get(issues_url)
 issues_data = issues_response.json()
 
+beginner_labels = [
+    "good first issue",
+    "help wanted"
+]
+found_issues = False
 count = 0
 
 for issue in issues_data:
     if "pull_request" in issue:
         continue
 
+    reasons = []
     is_beginner = False
     for label in issue["labels"]:
-        if label["name"] == "good first issue":
+        if label["name"] in beginner_labels:
             is_beginner = True
+            reasons.append(label["name"])
     if is_beginner:
-        print("GOOD FIRST ISSUE FOUND")
+        found_issues = True
+        print("Issue:")
         print(issue["title"])
+
+        print("Reason:")
+        for reason in reasons:
+            print("✓", reason)
+
+        print("Labels:")
+        for label in issue["labels"]:
+            print(label["name"])
+
+        print("Link:")
         print(issue["html_url"])
     count = count + 1
     if count == 5:
         break
+
+if not found_issues:
+    print("No beginner-friendly issues found.")
+    print("Try another repository.")
+
 print("Repository:", data["name"])
 print("Language:", data["language"])
 print("Stars:", data["stargazers_count"])
