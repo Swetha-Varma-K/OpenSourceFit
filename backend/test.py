edd4e02f457f97948f711a0322a17465cc2266a8
Swetha Varma <swethakv05@gmail.com>
2026-06-12 12:58:53 +0530

    Added beginner-friendly issue filtering

--- backend/test.py
+++ backend/test.py
@@ -6,7 +6,27 @@ url = f"https://api.github.com/repos/{repo}"
 response = requests.get(url)
 
 data = response.json()
+issues_url = f"https://api.github.com/repos/{repo}/issues"
+issues_response = requests.get(issues_url)
+issues_data = issues_response.json()
 
+count = 0
+
+for issue in issues_data:
+    if "pull_request" in issue:
+        continue
+
+    is_beginner = False
+    for label in issue["labels"]:
+        if label["name"] == "good first issue":
+            is_beginner = True
+    if is_beginner:
+        print("GOOD FIRST ISSUE FOUND")
+        print(issue["title"])
+        print(issue["html_url"])
+    count = count + 1
+    if count == 5:
+        break
 print("Repository:", data["name"])
 print("Language:", data["language"])
 print("Stars:", data["stargazers_count"])
