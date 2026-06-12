91bcf1810d16b95818c3fbb6605bfbe4615bb351
Swetha Varma <swethakv05@gmail.com>
2026-06-12 15:58:29 +0530

    Added basic skill matching

--- backend/test.py
+++ backend/test.py
@@ -1,14 +1,21 @@
 import requests
 
 repo = input("Enter repository (owner/repo): ")
+skill = input("Enter your skill: ")
+skills_list = skill.lower().split(",")
 url = f"https://api.github.com/repos/{repo}"
 
 response = requests.get(url)
 
 data = response.json()
-issues_url = f"https://api.github.com/repos/{repo}/issues"
+if skill.lower() == str(data["language"]).lower():
+    match_score = 100
+else:
+    match_score = 0
+issues_url = f"https://api.github.com/repos/{repo}/issues?per_page=100"
 issues_response = requests.get(issues_url)
 issues_data = issues_response.json()
+print("Issues received:", len(issues_data))
 
 beginner_labels = [
     "good first issue",
@@ -34,16 +41,13 @@ for issue in issues_data:
         found_issues = True
         recommended_count = recommended_count + 1
 
+        print("Issue Number:", issue["number"])
         print("Issue:")
         print(issue["title"])
 
         print("Reason:")
         for reason in reasons:
             print("✓", reason)
-
-        print("Labels:")
-        for label in issue["labels"]:
-            print(label["name"])
 
         print("Link:")
         print(issue["html_url"])
@@ -56,7 +60,7 @@ if not found_issues:
     print("Try another repository.")
 
 print("Recommendations Found:", recommended_count)
-
+print("Match Score:", match_score, "%")
 print("Repository:", data["name"])
 print("Language:", data["language"])
 print("Stars:", data["stargazers_count"])
